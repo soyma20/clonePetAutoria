63dbd31057b0d082a128248a8268170c9a7b2148
soyma20 <slaviksoyma2014@gmail.com>
2022-10-26 13:10:45 +0300

    feat: add models

---
+++ src/main/java/com/example/clonepetautoria/models/CarModel.java
@@ -0,0 +1,39 @@
+package com.example.clonepetautoria.models;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class CarModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String description;
+    private int year;
+    private int price;
+    @Enumerated(value = EnumType.STRING)
+    private Currency currency;
+    @OneToMany(cascade = CascadeType.ALL)
+    @JoinTable(
+            name= "car_images",
+            joinColumns = @JoinColumn(name = "car_id"),
+            inverseJoinColumns = @JoinColumn(name = "image_id")
+    )
+    private List<ImagePath> imagesPath = new ArrayList<>(); //name of file
+
+    public CarModel(String description, int year, int price) {
+        this.description = description;
+        this.year = year;
+        this.price = price;
+    }
+
+    public void addImagePath(ImagePath imagePath) {
+        this.imagesPath.add(imagePath);
+    }
+}
