38fbaa9b325906a240a67d4b97a9eb24d43a74fb
soyma20 <slaviksoyma2014@gmail.com>
2022-10-26 13:10:13 +0300

    feat: add enums

---
+++ src/main/java/com/example/clonepetautoria/models/Currency.java
@@ -0,0 +1,5 @@
+package com.example.clonepetautoria.models;
+
+public enum Currency {
+    USD, UAH, EUR
+}
---
+++ src/main/java/com/example/clonepetautoria/models/Roles.java
@@ -0,0 +1,5 @@
+package com.example.clonepetautoria.models;
+
+public enum Roles {
+    BUYER, USER, MANAGER, ADMINISTRATOR
+}
